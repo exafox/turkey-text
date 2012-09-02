19533545fd9739e52df6a2ecfb71475bc0be6443
Stefan Fox <sfox@barackobama.com>
2012-09-01 19:25:13 -0500

    Exports take base filename

--- turkey.py
+++ turkey.py
@@ -1,12 +1,15 @@
 import sublime,sublime_plugin
 import os,sys,subprocess,threading
 from datetime import datetime
-import json
+import json,re
+from functools import partial
+sql_comments = re.compile('\/\*.+?\*\/')
 
 #Example localsettings.json
 #{
+#    "ROOT":"/path/to/output/folder",
 #    "SQL_COMMAND":"sqlcmd -h db.some.domain -U jerkface -w securityftw"],
-#    "SQL_EXPORT_COMMAND":"-o /home/user/sqldocs/{outfile}"
+#    "SQL_EXPORT_COMMAND":"-o {outfile}" //path based off ROOT dir
 #}
 
 settings = json.loads(open('localsettings.json').read())
@@ -30,15 +33,18 @@ def sql_proc(data=None,export=False):
 
 class SqlCall(threading.Thread):
 
-    def __init__(self, sql_statements, export):
+    def __init__(self, sql_statements, export, slug):
         self.sql_statements = sql_statements
         self.export = export
+        self.slug = slug
         threading.Thread.__init__(self)
+
 
     def run(self):       
         # settings = json.loads(open('localsettings.json').read())
         self.data = list() 
         for statement in self.sql_statements:
+
             is_select = statement.strip().startswith('select')
 
             if not statement.strip().endswith(';'):
@@ -49,7 +55,10 @@ class SqlCall(threading.Thread):
                 statement = statement.rstrip(';') + '\nlimit 1000;'
             statement += ' commit;'
             statement = statement
-            fname = datetime.now().time().isoformat() + '.csv'
+            if self.slug:
+                fname = os.path.join(settings['ROOT'],'%s_%s.csv' % (self.slug,datetime.now().time().isoformat()))
+            else:
+                fname = datetime.now().time().isoformat() + '.csv'
             proc = sql_proc(data=dict(outfile=fname),export=is_select and self.export)
             tstamp = datetime.now()
             out,err = proc.communicate(statement)
@@ -68,7 +77,7 @@ class SqlCommand(sublime_plugin.TextCommand):
         self.offset += len(data)
     def run(self,edit):
         self._run(edit)
-    def _run(self, edit, export=False):
+    def _run(self, edit, export=False,slug=None):
         """
             Runs selected code in isql using the named DSN.
         """
@@ -78,21 +87,24 @@ class SqlCommand(sublime_plugin.TextCommand):
         self.edit = edit
         self.offset = 0 
 
-        #clean code two ways
+        #strip comments & clean code two ways
         sql_statements = '\n'.join([self.view.substr(region) for region in self.view.sel()])
+        for comment in sql_comments.findall(sql_statements):
+            sql_statements = sql_statements.replace(comment,'')
+        sql_statements = '\n'.join([line.split('--').pop(0) for line in sql_statements.split('\n')])
         for banned_word in banned_words:
             sql_statements = sql_statements.replace(banned_word,'')
         sql_statements = [x + ';' for x in sql_statements.split(';') if x.strip() != '']
 
         #immediate feedback confirms somethings happening
         if export:
-            self.log("%s-Turkey-Export.log\n\n" % datetime.now().time().strftime('%H:%M'))
+            self.log("%s-Turkey-%s.log\n\n" % (datetime.now().time().strftime('%H:%M'),slug))
         else:
             self.log("%s-Turkey.log\n\n" % datetime.now().time().strftime('%H:%M'))
         self.log(' > ' + ''.join(sql_statements).replace('\n','\n > ') + '\n\n')
 
         #run some things
-        caller = SqlCall(sql_statements,export)
+        caller = SqlCall(sql_statements,export,slug)
         caller.start()
         threads = [caller]
         self.log_view.end_edit(self.edit)
@@ -115,10 +127,13 @@ class SqlCommand(sublime_plugin.TextCommand):
                 sublime.set_timeout(lambda: self.thread_handler(not_finished),1000)
             else:
                 self.log('\n[done]')
-
-
+def nothing(val):
+    pass
 class SqlexportCommand(SqlCommand):
     def run(self,edit):
-        SqlCommand._run(self,edit,export=True)
+        self.edit = edit
+        self.view.window().show_input_panel('Name this export fool', 'Export', partial(self._run), nothing, nothing)
+    def _run(self,val):
+        SqlCommand._run(self,self.edit,export=True,slug=val)
 
 
